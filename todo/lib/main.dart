ad8d5f6f161ab341e8a091c4dd81250879ba53c4
Pankaj <pankajkryadav90062@gmail.com>
2024-06-25 10:50:48 +0530

    ToDo List in Flutter

---
+++ todo/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo/homeScreen/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo List',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
